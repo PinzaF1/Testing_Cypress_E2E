6bc5ddbbbb568bd7e5692f9dbb8e145b15748057
Daniel Diaz <carlosdanielpinzadiaz@gmail.com>
2025-11-19 16:46:45 -0500

    si

--- cypress/e2e/Listar_usuario.cy.js
+++ cypress/e2e/Listar_usuario.cy.js
@@ -6,7 +6,11 @@ describe("listar usuario",()=>{
         "https://skojryaxbquqtwvuyhfv.supabase.co/rest/v1/users"
     ).as("getUsuario");
 
-    cy.get("table tbody tr").should("exist");
-    cy.get("table tbody tr").its("length").should("",0);
+        cy.get("table tbody tr").should("exist");
+        cy.get("table tbody tr").its("length").should("be.gt",0);
+        cy.get("table tbody tr").each(($tr)=>{
+            cy.wrap($tr).find("td").eq(0).should("not.be.empty");
+            cy.wrap($tr).find("td").eq(1).should("not.be.empty");
+       })
     })
 })
